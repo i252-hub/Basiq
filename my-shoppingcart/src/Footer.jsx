63e74aeb6618b8e61450de9c4f2b98953e3f3160
i252-hub <jushirarey@gmail.com>
2025-02-26 20:08:37 +0800

    style: fontsize, positioning, grid & flex layout, stacking

---
+++ my-shoppingcart/src/Footer.jsx
@@ -0,0 +1,22 @@
+const Footer = () =>{
+
+    return(
+        <footer className="footer">
+            <div className="footerlinkscontainer">
+                <div className="footerlinks">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="policylinks">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+            <div className="copyright"></div>
+        </footer>
+    )
+}
+
+export default Footer;
