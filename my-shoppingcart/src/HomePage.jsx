eddfe2e6b09dcef1879dd32ce6832ffa12ec0d34
i252-hub <jushirarey@gmail.com>
2025-02-26 20:07:11 +0800

    style: new collection section, newsletter section, and footer layout

--- my-shoppingcart/src/HomePage.jsx
+++ my-shoppingcart/src/HomePage.jsx
@@ -1,6 +1,6 @@
 import './styles/HomePage.css';
 import Navbar from './Nav'
-
+import Footer from './Footer'
 const HomePage = () => {
     return (
         <>
@@ -31,8 +31,122 @@ const HomePage = () => {
         </div>
        
        <section className='newCollection'>
-
+        <div className='textcollection'>
+            <div className='collectiontitle'>
+                <h1>NEW COLLECTION</h1>
+            </div>
+            <div className='collectionsubtitle'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            </div>
+        </div>
+        <div className='extra'>
+        <div className='collectionitemscontainer'>
+            <div className='collectionitem one'>
+                <div className='imagecollectioncontainer'></div>
+                <div className='collectiondetails'>
+                    <p className='productcollectiontitle'>Lorem Ipsum</p>
+                    <p className='productcollectiondesc'>consectetur adipiscing elit</p>
+                    <p className='productcollectionprice'>$95</p>
+                </div>
+            </div>
+            <div className='collectionitem two'>
+            <div className='imagecollectioncontainer'></div>
+            <div className='collectiondetails'>
+                    <p className='productcollectiontitle'>Lorem Ipsum</p>
+                    <p className='productcollectiondesc'>consectetur adipiscing elit</p>
+                    <p className='productcollectionprice'>$95</p>
+                </div>
+            </div>
+            <div className='collectionitem three'>
+            <div className='imagecollectioncontainer'></div>
+            <div className='collectiondetails'>
+                    <p className='productcollectiontitle'>Lorem Ipsum</p>
+                    <p className='productcollectiondesc'>consectetur adipiscing elit</p>
+                    <p className='productcollectionprice'>$95</p>
+                </div>
+            </div>
+            <div className='collectionitem four'>
+            <div className='imagecollectioncontainer'></div>
+            <div className='collectiondetails'>
+                    <p className='productcollectiontitle'>Lorem Ipsum</p>
+                    <p className='productcollectiondesc'>consectetur adipiscing elit</p>
+                    <p className='productcollectionprice'>$95</p>
+                </div>
+            </div>
+            <div className='collectionitem five'>
+            <div className='imagecollectioncontainer'></div>
+            <div className='collectiondetails'>
+                    <p className='productcollectiontitle'>Lorem Ipsum</p>
+                    <p className='productcollectiondesc'>consectetur adipiscing elit</p>
+                    <p className='productcollectionprice'>$95</p>
+                </div>
+            </div>
+            <div className='collectionitem six'>
+            <div className='imagecollectioncontainer'></div>
+            <div className='collectiondetails'>
+                    <p className='productcollectiontitle'>Lorem Ipsum</p>
+                    <p className='productcollectiondesc'>consectetur adipiscing elit</p>
+                    <p className='productcollectionprice'>$95</p>
+                </div>
+            </div>
+        </div>
+        </div>
        </section>
+
+       <section className='catalog'>
+        <div className='catalogcontainer'>
+            <div className='catalogone'>
+                {/*image*/ }
+                <div className='cover'>
+                    {/*show text on hover:
+                    <p>Men's Catalog</p>
+                    */}
+                    
+                </div>
+            </div>
+            <div className='catalogtwo'>
+                 {/*image*/ }
+                 <div className='cover'>
+                   {/*show text on hover:
+                    <p>Women's Catalog</p>
+                    */}
+                 </div>
+            </div>
+            <div className='catalogthree'>
+                 {/*image*/ }
+                 <div className='cover'>
+                {/*show text on hover:
+                    <p>Accessories' Catalog</p>
+                    */}
+                 </div>
+            </div>
+        </div>
+       </section>
+
+       <section className='newsletter'>
+        <div className='newslettercontainer'>
+            <div className='newstitle'>
+                <p>EXCLUSIVE NEWS & CONTENT</p>
+            </div>
+            <div className='newssubtitle'>
+            <p>Proin fringilla nisi ut nisl consectetur, ut <br></br>
+            consectetur eros cursus</p>
+            </div>
+            <div className='newssignup'>
+            <div className='newsinputcontainer'>
+                <input></input>
+                <div className='newsbtncontainer'>
+                <button>
+                    <p> Sign Up</p>
+                   </button>
+                </div>
+               
+            </div>
+            </div>
+        </div>
+       </section>
+       <Footer/>
+
        </div>
         </>
        
