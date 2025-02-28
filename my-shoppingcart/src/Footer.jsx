4c3aa0ffd1bf8f86a1da7c947a5bc8afe612ae28
i252-hub <jushirarey@gmail.com>
2025-02-28 10:42:55 +0800

    added footer links and copyright, styled ui layout

--- my-shoppingcart/src/Footer.jsx
+++ my-shoppingcart/src/Footer.jsx
@@ -4,17 +4,64 @@ const Footer = () =>{
         <footer className="footer">
             <div className="footerlinkscontainer">
                 <div className="footerlinks">
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div className="footerlinksone">
+                        <div className="titlefooterlink">
+                            <p>SERVICE</p>
+                        </div>
+                        <div>
+                            <p>Shop</p>
+                        </div>
+                        <div>
+                            <p>About</p>
+                        </div>
+                        <div>
+                            <p>Contact Us</p>
+                        </div>
+                    </div>
+                    <div className="footerlinkstwo">
+                    <div className="titlefooterlink">
+                            <p>CATEGORIES</p>
+                        </div>
+                        <div>
+                            <p>Men</p>
+                        </div>
+                        <div>
+                            <p>Women</p>
+                        </div>
+                        <div>
+                            <p className="acc">Accessories</p>
+                        </div>
+                    </div>
+                    <div className="footerlinksthree">
+                    <div className="titlefooterlink">
+                            <p>SOCIAL</p>
+                        </div>
+                        <div>
+                            <p>Facebook</p>
+                        </div>
+                        <div>
+                            <p>Instagram</p>
+                        </div>
+                        <div className="footerlinksthreesocial">
+                            <p></p>
+                        </div>
+                    </div>
                 </div>
                 <div className="policylinks">
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div className="policylinksone">
+                        <p>Terms and Conditions</p>
+                    </div>
+                    <div className="policylinkstwo">
+                    <p>Privacy Policy</p>
+                    </div>
+                    <div className="policylinksthree">
+                    <p>Cookie Policy</p>
+                    </div>
                 </div>
             </div>
-            <div className="copyright"></div>
+            <div className="copyright">
+                <p>  © 2025 i252-hub All rights reserved</p>
+               </div>
         </footer>
     )
 }
