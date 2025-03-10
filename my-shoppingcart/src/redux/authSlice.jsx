4e8824185cbc2493175712123431791eaef73e63
i252-hub <jushirarey@gmail.com>
2025-03-10 14:34:09 +0800

    feat: login authentication

---
+++ my-shoppingcart/src/redux/authSlice.jsx
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    user: JSON.parse(localStorage.getItem("loggedInUser")) || null,
+};
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.user = action.payload;
+            localStorage.setItem("loggedInUser", JSON.stringify(action.payload));
+        },
+        logout: (state) => {
+            state.user = null;
+            localStorage.removeItem("loggedInUser");
+        },
+    },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
--- my-shoppingcart/src/redux/store.jsx
+++ my-shoppingcart/src/redux/store.jsx
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice"; 
 import wishlistReducer from "./wishlistSlice";  
+import authReducer from "./authSlice";
+
 
 
 const store = configureStore({
   reducer: {
     cart: cartReducer, 
     wishlist: wishlistReducer,  
+    auth: authReducer,
 
   },
 });
