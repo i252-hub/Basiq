ee651e40e916ec1742b90a249bfb7e5bd10e8def
i252-hub <jushirarey@gmail.com>
2025-03-12 15:31:33 +0800

    feat: import section to access globally

--- my-shoppingcart/src/main.jsx
+++ my-shoppingcart/src/main.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import routes from './routes';
+import ScrollToSection from "./Section";
 
 const Main = () => {
   return (
     <Provider store={store}>
       <Router>
+      <ScrollToSection />
         <Routes>
           {routes.map(({ path, element }, idx) => (
             <Route key={idx} path={path} element={element} />
